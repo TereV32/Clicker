042bc7e8d474be860db3870cd7dc0ef7f2c27692
tvill08 <63677685+TereV32@users.noreply.github.com>
2023-03-01 09:07:31 -0600

    Initial Commit

---
+++ Clicker/ClickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickerApp.swift
+//  Clicker
+//
+//  Created by Teresa Villegas on 3/1/23.
+//
+
+import SwiftUI
+
+@main
+struct ClickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
