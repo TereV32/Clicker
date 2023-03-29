0b665ca48f3de4d84e89e163bb38edefdb2da86a
tvill08 <63677685+TereV32@users.noreply.github.com>
2023-03-29 15:23:40 -0500

    Delete buttons

--- Clicker/ContentView.swift
+++ Clicker/ContentView.swift
@@ -6,52 +6,52 @@
 //
 
 import SwiftUI
+import SpriteKit
+import EffectsLibrary
 
 struct ContentView: View {
-    @State private var count = 0
+    
+    @State public var count = 0
     @State private var button1Count = 0
     @State private var button2Count = 0
     @State private var button3Count = 0
+    
+    let rainEmitter = SKEmitterNode(fileNamed: "Rain.sks")!
+
+    var rainScene: SKScene {
+        let scene = RainScene.shared
+        scene.size = UIScreen.screenSize
+        scene.scaleMode = .fill
+        scene.backgroundColor = .clear
+        return scene
+    }
 
     var body: some View {
-        VStack {
-            Spacer()
-            
-            Text("Total # of clicks: \(count)")
-            
-            Spacer()
-            HStack {
-                Button {
-                    button1Count += 1
-                    count += 1
-                } label: {
-                    Text("Button 1")
+//        ZStack {
+//            SpriteView(scene: rainScene, options: .allowsTransparency)
+//                .frame(width: UIScreen.screenWidth, height: UIScreen.screenHeight)
+//
+            VStack {
+                Spacer()
+                
+                Text("Total # of clicks: \(count)")
+                
+                Spacer()
+                
+                HStack {
+                    Button {
+                        button1Count += 1
+                        count += 1
+                    } label: {
+                        Text("Button 1")
+                    }
+                    Text("Button 1: \(button1Count)")
                 }
-                Text("Button 1: \(button1Count)")
+                Spacer()
             }
-            HStack {
-                Button {
-                    button2Count += 1
-                    count += 1
-                } label: {
-                    Text("Button 2")
-                }
-                Text("Button 2: \(button2Count)")
-            }
-            HStack {
-                Button {
-                    button3Count += 1
-                    count += 1
-                } label: {
-                    Text("Button 3")
-                }
-                Text("Button 3: \(button3Count)")
-            }
-
-        }
+//        }
         .padding()
     }
-    
 //    func createResourceLabel(_ resource: String, _ resourceAction: String) -> some View {
 //        HStack {
 //            Image("\(resource)")
@@ -71,3 +71,10 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+extension UIScreen {
+    static let screenWidth = UIScreen.main.bounds.size.width
+    static let screenHeight = UIScreen.main.bounds.size.height
+    static let screenSize = UIScreen.main.bounds.size
+}
+
