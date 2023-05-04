0932bfbc1dc4b6a291292946260a227187c7f5e9
tvill08 <63677685+TereV32@users.noreply.github.com>
2023-05-04 11:19:25 -0500

    Fixed the upgrade counts

--- Clicker/CubeGridView.swift
+++ Clicker/CubeGridView.swift
@@ -13,7 +13,7 @@ struct CubeView: View {
     @ObservedObject var gameState : GameState
     let cubeValue : Int
     @State var isSelected = false
-    
+        
     var body: some View {
             RoundedRectangle(cornerRadius: 5)
                 .fill(.brown)
@@ -22,33 +22,19 @@ struct CubeView: View {
                 .onTapGesture {
                     gameState.firstWaterMessage = false
                     isSelected = true
-                    gameState.cubeClicked = cubeValue
+//                    gameState.cubeClicked = cubeValue
                     upgrade()
                     print(gameState.currentCube?.imageName ?? "")
                 }
         VStack {
-//            if isSelected && !gameState.upgradeWaterComplete {
-//                collectBarView(gameState: gameState, barWidth: CGFloat(gameState.upgradeNumArray[cubeValue]))
-//                    .onDisappear {
-//                        isSelected = false
-//                    }
-//            } else if isSelected && !gameState.upgradeSunComplete {
-//                collectBarView(gameState: gameState, barWidth: CGFloat(gameState.upgradeNumArray[cubeValue]))
-//                    .onDisappear {
-//                        isSelected = false
-//                    }
-//            } else if isSelected && !gameState.upgradeGrassComplete {
-//                collectBarView(gameState: gameState, barWidth: CGFloat(gameState.upgradeNumArray[cubeValue]))
-//                    .onDisappear {
-//                        isSelected = false
-//                    }
-//            }
-            if isSelected && gameState.upgradeNumArray[cubeValue] != 0 {
+            if (gameState.upgradeNumArray[cubeValue] > 0 && gameState.cubeColorsArray[cubeValue] == "drop.fill"){
                 collectBarView(gameState: gameState, resourceCount: gameState.upgradeNumArray[cubeValue], numToUpgrade: gameState.cubeColors[2].numToUpgrade)
-                    .onDisappear {
-                        isSelected = false
-                    }
+            } else if (gameState.upgradeNumArray[cubeValue] > 0 && gameState.cubeColorsArray[cubeValue] == "sun.max.fill"){
+                collectBarView(gameState: gameState, resourceCount: gameState.upgradeNumArray[cubeValue], numToUpgrade: gameState.cubeColors[3].numToUpgrade)
+            } else if (gameState.upgradeNumArray[cubeValue] > 0 && gameState.cubeColorsArray[cubeValue] == "leaf.fill"){
+                collectBarView(gameState: gameState, resourceCount: gameState.upgradeNumArray[cubeValue], numToUpgrade: gameState.cubeColors[4].numToUpgrade)
             }
+            
             if !gameState.flowers.contains(gameState.cubeColorsArray[cubeValue]) {
                 Image(systemName: gameState.cubeColorsArray[cubeValue])
                     .foregroundColor(
@@ -66,13 +52,16 @@ struct CubeView: View {
     // Function to see what needs to be upgrade
     func upgrade() {
         if gameState.cubeColorsArray[cubeValue] == "drop.fill" {
-            gameState.getSunCube(cubeColor: self.gameState.cubeColors[2])
+            gameState.getSunCube(cubeColor: self.gameState.cubeColors[2], cubeClicked: cubeValue)
             print("test")
         } else if gameState.cubeColorsArray[cubeValue] == "sun.max.fill" {
-            gameState.getGrassCube(cubeColor: self.gameState.cubeColors[3])
+            gameState.getGrassCube(cubeColor: self.gameState.cubeColors[3], cubeClicked: cubeValue)
         } else if gameState.cubeColorsArray[cubeValue] == "leaf.fill" {
-            gameState.getPlantCube(cubeColor: self.gameState.cubeColors[4])
+            gameState.getPlantCube(cubeColor: self.gameState.cubeColors[4], cubeClicked: cubeValue)
+        } else if gameState.flowers.contains(gameState.cubeColorsArray[cubeValue]) {
+            gameState.collectFlower(cubeClicked: cubeValue)
         }
+        isSelected = false
     }
 }
 
@@ -107,41 +96,9 @@ struct CubeGridView: View {
     }
 }
 
-struct LevelGridView: View {
-    
-    @ObservedObject var gameState : GameState
-    
-    var body: some View {
-        ScrollView {
-            Grid {
-                ForEach(1..<7, id: \.self) { row in
-                    GridRow {
-                        ForEach(1..<4, id: \.self) { column in
-                            let value = (row * 3 + column)-3
-                            ZStack {
-                                CubeGridView(gameState: gameState)
-                                if gameState.gridsUnlocked <= value {
-                                    Image(systemName: "lock.fill")
-                                        .resizable()
-                                        .frame(width: 60, height: 60)
-                                }
-                            }
-                            .onAppear {
-                                print("value \(value)")
-                                print("gridsUnlocked \(gameState.gridsUnlocked)")
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
 
 struct CubeView_Previews: PreviewProvider {
     static var previews: some View {
         CubeGridView(gameState: GameState())
-        
-        LevelGridView(gameState: GameState())
     }
 }
--- Clicker/Generators /GameState.swift
+++ Clicker/Generators /GameState.swift
@@ -11,25 +11,14 @@ import SwiftUI
 class GameState : ObservableObject {
     // Declares the Variables
     // Published prop. allows other views to react to changes
-    @Published var waterCount = 0
-    @Published var sunCount = 0
-    @Published var grassCount = 0
     @Published var buttonClickedNum = 0
     
     @Published var currentCube : CubeColor?
-    @Published var cubeClicked = 0
     @Published var cubeColorsArray: [String] = Array(repeating: "", count: 9)
     @Published var upgradeNumArray: [Int] = Array(repeating: 0, count: 9)
     
     @Published var gridFull = false
-    @Published var gridsUnlocked = 3
     @Published var cubeValue = 0
-    
-    @Published var upgradeWaterComplete = false
-    @Published var upgradeSunComplete = false
-    @Published var upgradeGrassComplete = false
-
-    @Published var numflowersCollected = 0
     
     @Published var flowers = ["Flower1", "Flower2", "Flower3", "Flower4", "Flower5", "Flower6"]
     
@@ -60,100 +49,87 @@ class GameState : ObservableObject {
     }
     
     // Function that collects water and when meets requirements changes cube to sun
-    func getSunCube(cubeColor: CubeColor) {
-        cubeColorsArray.remove(at: cubeClicked)
-        let blueIndices = cubeColorsArray.indices.filter { cubeColorsArray[$0] == "drop.fill" }
-        if let randomIndex = blueIndices.randomElement() {
-            let resourceCount = upgradeNumArray[randomIndex]
-            if resourceCount > 0 {
-                
-                let collectedResources = upgradeNumArray[cubeClicked]
-                print("res \(resourceCount), coll \(collectedResources), amount now \(resourceCount + collectedResources)")
-                upgradeNumArray.insert(resourceCount + collectedResources, at: cubeClicked)
-                upgradeNumArray.insert(0, at: randomIndex)
-            }
-            cubeColorsArray[randomIndex] = ""
-        }
-        
-        
-        
+    func getSunCube(cubeColor: CubeColor, cubeClicked: Int) {
+        var currentWaterCount = upgradeNumArray[cubeClicked]
+        let blueIndices = cubeColorsArray.indices.filter { $0 != cubeClicked && cubeColorsArray[$0] == "drop.fill" }
         if !blueIndices.isEmpty {
-            upgradeNumArray[cubeClicked] += 1
-            if upgradeNumArray[cubeClicked] >= cubeColor.numToUpgrade {
-                cubeColorsArray.insert("sun.max.fill", at: cubeClicked)
-                upgradeNumArray.insert(0, at: cubeClicked)
-            } else {
-                cubeColorsArray.insert("drop.fill", at: cubeClicked)
+            let randomIndex = blueIndices.randomElement()!
+            if upgradeNumArray[randomIndex] > 0 {
+                currentWaterCount += upgradeNumArray[randomIndex]
+                upgradeNumArray[randomIndex] = 0
             }
+            upgradeNumArray[cubeClicked] = currentWaterCount + 1
+            cubeColorsArray[randomIndex] = ""
+            
+            if upgradeNumArray[cubeClicked] >= cubeColor.numToUpgrade {
+                cubeColorsArray[cubeClicked] = "sun.max.fill"
+                upgradeNumArray[cubeClicked] = 0
+            } else {
+                cubeColorsArray[cubeClicked] = "drop.fill"
+            }
+            print(blueIndices)
+            print(upgradeNumArray)
         }
     }
     
     
     // Function that collects sun and when meets requirements changes cube to a grass
-    func getGrassCube(cubeColor: CubeColor) {
-        cubeColorsArray.remove(at: cubeClicked)
-        let yellowIndices = cubeColorsArray.indices.filter { cubeColorsArray[$0] == "sun.max.fill" }
-        if let randomIndex = yellowIndices.randomElement() {
-            cubeColorsArray[randomIndex] = ""
-            if upgradeNumArray[randomIndex] != 0 {
-                upgradeNumArray[cubeClicked] = upgradeNumArray[randomIndex]
+    func getGrassCube(cubeColor: CubeColor, cubeClicked: Int) {
+        var currentSunCount = upgradeNumArray[cubeClicked]
+        
+        let yellowIndices = cubeColorsArray.indices.filter { $0 != cubeClicked && cubeColorsArray[$0] == "sun.max.fill" }
+        if !yellowIndices.isEmpty {
+            let randomIndex = yellowIndices.randomElement()!
+            if upgradeNumArray[randomIndex] > 0 {
+                currentSunCount += upgradeNumArray[randomIndex]
                 upgradeNumArray[randomIndex] = 0
-                print(upgradeNumArray)
             }
+            upgradeNumArray[cubeClicked] = currentSunCount + 1
+            cubeColorsArray[randomIndex] = ""
         }
         
-        if !yellowIndices.isEmpty {
-            upgradeNumArray[cubeClicked] += 1
-            print(upgradeNumArray)
-        }
         if upgradeNumArray[cubeClicked] >= cubeColor.numToUpgrade {
-            cubeColorsArray.insert("leaf.fill", at: cubeClicked)
+            cubeColorsArray[cubeClicked] = "leaf.fill"
             upgradeNumArray[cubeClicked] = 0
         } else {
-            cubeColorsArray.insert("sun.max.fill", at: cubeClicked)
+            cubeColorsArray[cubeClicked] = "sun.max.fill"
         }
+        print(upgradeNumArray)
         
     }
     
     
     // Function that collects grass and when meets requirements changes cube to a plant
-    func getPlantCube(cubeColor: CubeColor) {
-        cubeColorsArray.remove(at: cubeClicked)
-        let greenIndices = cubeColorsArray.indices.filter { cubeColorsArray[$0] == "leaf.fill" }
-        if let randomIndex = greenIndices.randomElement() {
-            cubeColorsArray[randomIndex] = ""
-            if upgradeNumArray[randomIndex] != 0 {
-                upgradeNumArray[cubeClicked] = upgradeNumArray[randomIndex]
-                upgradeNumArray[randomIndex] = 0
-                print(upgradeNumArray)
-            }
-            
-        }
+    func getPlantCube(cubeColor: CubeColor, cubeClicked: Int) {
+        var currentGrassCount = upgradeNumArray[cubeClicked]
         
+        let greenIndices = cubeColorsArray.indices.filter { $0 != cubeClicked && cubeColorsArray[$0] == "leaf.fill" }
         if !greenIndices.isEmpty {
-            upgradeNumArray[cubeClicked] += 1
+            let randomIndex = greenIndices.randomElement()!
+            if upgradeNumArray[randomIndex] > 0 {
+                currentGrassCount += upgradeNumArray[randomIndex]
+                upgradeNumArray[randomIndex] = 0
+            }
+            upgradeNumArray[cubeClicked] = currentGrassCount + 1
+            cubeColorsArray[randomIndex] = ""
+            
+            if upgradeNumArray[cubeClicked] >= cubeColor.numToUpgrade {
+                let randomElement = flowers.randomElement()
+                cubeColorsArray[cubeClicked] = randomElement!
+                if !flowersUnlocked.contains(randomElement!) {
+                    flowersUnlocked.append(randomElement!)
+                }
+                upgradeNumArray[cubeClicked] = 0
+            } else {
+                cubeColorsArray[cubeClicked] = "leaf.fill"
+            }
             print(upgradeNumArray)
         }
-        if upgradeNumArray[cubeClicked] >= cubeColor.numToUpgrade {
-            let randomElement = flowers.randomElement()
-            cubeColorsArray.insert(randomElement!, at: cubeClicked)
-            if !flowersUnlocked.contains(randomElement!) {
-                flowersUnlocked.append(randomElement!)
-            }
-            print(cubeColorsArray)
-            upgradeGrassComplete = true
-            upgradeNumArray[cubeClicked] = 0
-        } else {
-            cubeColorsArray.insert("leaf.fill", at: cubeClicked)
-        }
-        
     }
     
     // Collect Flower
-    func collectFlower(cubeColor: CubeColor) {
+    func collectFlower(cubeClicked: Int) {
         cubeColorsArray.insert("", at: cubeClicked)
-        numflowersCollected += 1
-        gridsUnlocked += 1
     }
     
     
@@ -164,6 +140,5 @@ class GameState : ObservableObject {
         return !cubeColorsArray.contains("")
     }
     
-    
-    
 }
+
